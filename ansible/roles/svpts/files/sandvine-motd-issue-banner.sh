8187cc555dbe518257e8ed8fb1d4b2efe6261e7d
Thiago Martins <tmartins@sandvine.com>
2016-05-26 04:20:17 +0000

    Adding figlet and scripts to create login banners

---
+++ ansible/roles/svpts/files/sandvine-motd-issue-banner.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# $1 = name
+setMotdIssue() {
+	figlet -f slant $1 > /etc/motd
+	figlet -f slant $1 | sed -e 's/\\/\\\\/g' > /etc/issue
+}
+
+if [ -s "/usr/local/sandvine/etc/versions/PTS" ]; then
+	setMotdIssue "Sandvine - PTS"
+fi
