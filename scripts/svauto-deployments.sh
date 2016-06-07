f089d4cbacc14699651343ca0318937594c40005
Thiago Martins <tmartins@sandvine.com>
2016-06-07 05:09:56 +0000

    Adding new install script helper
    We have a new script, the scripts/svauto-deployments.sh, it makes use of the
    ansible_playbook_builder function to dynamically generate the playbooks.
    
    It also supports Ansible's Extra Vars.

---
+++ scripts/svauto-deployments.sh
@@ -0,0 +1,116 @@
+#! /bin/bash
+
+# Copyright 2016, Sandvine Incorporated.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+clear
+
+
+for i in "$@"
+do
+case $i in
+
+	--base-os=*)
+
+	        BASE_OS="${i#*=}"
+	        shift
+	        ;;
+
+	--roles=*)
+
+	        ALL_ROLES="${i#*=}"
+	        ROLES="$( echo $ALL_ROLES | sed s/,/\ /g )"
+	        shift
+	        ;;
+
+	--extra-vars=*)
+
+	        ALL_EXTRA_VARS="${i#*=}"
+	        EXTRA_VARS="$( echo $ALL_EXTRA_VARS | sed s/,/\ /g )"
+	        shift
+	        ;;
+
+esac
+done
+
+
+BUILD_RAND=$(openssl rand -hex 4)
+
+PLAYBOOK_FILE="playbook-"$BUILD_RAND"-"$ALL_ROLES".yml"
+
+
+echo
+echo "Welcome to SVAuto, the Sandvine Automation!"
+
+
+echo
+echo "Installing SVAuto basic dependencies:"
+
+echo
+sudo apt -y install git ansible
+
+echo
+
+
+if  [ ! -d ~/svauto ]; then
+        echo
+        echo "Downloading SVAuto into your home directory..."
+        echo
+
+        cd ~
+        git clone -b dev https://github.com/tmartinx/svauto.git
+else
+        echo
+        echo "Apparently, you already have SVAuto, enjoy it!"
+        echo
+fi
+
+
+if  [ ! -f ~/svauto/svauto.sh ]; then
+	echo
+	echo "WARNING!"
+	echo "SVAuto main script not found, Git clone might have failed."
+
+	echo
+	exit 1
+fi
+
+
+echo
+echo "Loading SVAuto includes:"
+cd ~/svauto
+source lib/include-tools.inc
+
+
+echo
+echo "Bootstrapping --base-os=\"$BASE_OS\" via Ansible with the folllwing roles and vars:"
+echo
+echo "Roles: "$ALL_ROLES"."
+echo "Extra Vars: "$ALL_EXTRA_VARS"."
+
+
+echo
+echo "Building Ansible top-level Playbook:"
+
+echo
+ansible_playbook_builder --base-os="$BASE_OS" --ansible-hosts="localhost" --roles="$ALL_ROLES" > ansible/tmp/$PLAYBOOK_FILE
+
+
+echo
+echo "Running Ansible with Playbook: \"$PLAYBOOK_FILE\"."
+
+echo
+cd ~/svauto/ansible
+ansible-playbook -c local "tmp/$PLAYBOOK_FILE" --extra-vars "base_os=$BASE_OS $EXTRA_VARS"
